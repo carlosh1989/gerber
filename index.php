15c6040e1dfc623990990209989ebd9f9cce05d1
carlos silva <carloshsilva1989@gmail.com>
2018-11-23 10:49:28 -0500

    cambios de ultimo momento en la estructura de folder

--- index.php
+++ index.php
@@ -1,67 +1,25 @@
 <?php
-
-  		//Shorcode wordpress, para recibir los parametros
-  		$args = array(
-  			'link' => '#',
-            'url_img' => 'v1539205369/WEB/Home/centro-de-tecnologia.jpg', 
-            'alt' => 'descripcion de imagen');
-
-  		//asignacion de valores
-        $link = $args['link'];
-        $url_img = $args['url_img'];
-        $alt = $args['alt'];
-        $base_cloudinary = "https://res.cloudinary.com/webuvg/image/upload/";
-        $parametro_webP ="f_auto,q_auto/";
-        $parametro_compresion = "q_auto/fl_any_format/";
-        $parametro_W = $_POST['anchoDiv'];
-
-        var_dump($_REQUEST) ;
-
-
-        //detectar navegador version 2
-        function get_browser_name($user_agent)
-        {
-            if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'Opera';
-            elseif (strpos($user_agent, 'Edge')) return 'Edge';
-            elseif (strpos($user_agent, 'Chrome')) return 'Chrome';
-            elseif (strpos($user_agent, 'Safari')) return 'Safari';
-            elseif (strpos($user_agent, 'Firefox')) return 'Firefox';
-            elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) return 'Internet Explorer';
-           
-            return 'Other';
-        }
-        
-
-        if (get_browser_name($_SERVER['HTTP_USER_AGENT']) == 'Chrome') {
-            if ($link == '#') {
-                echo $img = "<div class='ancho' style='width:100%'><img src='".$base_cloudinary.$parametro_webP.$url_img."' alt='".$alt."'></div>";
-            }else{
-                echo $img = "<div class='ancho' style='width:100%'><a href='".$link."'><img src='".$base_cloudinary.$parametro_webP.$url_img."' alt='".$alt."'></a></div>";
-            }
-        }else{
-        	if ($link == '#') {
-                echo $img = "<div class='ancho' style='width:300px'><img src='".$base_cloudinary.$parametro_compresion.$url_img."' alt='".$alt."'></div>";
-            }else{
-                echo $img = "<div class='ancho' style='width:300px'><a href='".$link."'><img src='".$base_cloudinary.$parametro_compresion.$url_img."' alt='".$alt."'></a></div>";
-            }
-        }
-
-
-  ?>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script>
-    $(document).ready(function(){
-    var anchoDiv = $('.ancho').width();
-    $.ajax({
-        data: {"anchoDiv":anchoDiv},
-        url: 'index.php',
-        type: 'post',
-        success: function(response){
-            console.log(anchoDiv)
-        }
-    });
-});
-    </script>
-  <?php
-
+$args = array(
+    'link' => '#',
+    'url_img' => 'https://res.cloudinary.com/tuconsultaenlinea/image/upload/w_auto,c_scale/v1533602773/iwevpcagpd0kuwno3xnx.jpg', 
+    'alt' => 'descripcion de imagen');
 ?>
+
+<meta name="viewport" content="width=device-width">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+<? if ($args['link'] == '#'): ?>
+    <img data-src="<?php echo $args['url_img'] ?>" class="cld-responsive">
+<? else: ?>
+    <a href="<?php $args['link'] ?>"><img data-src="<?php echo $args['url_img'] ?>" class="cld-responsive"></a>
+<? endif; ?>
+
+
+ 
+<script src="bower_components/cloudinary-core/cloudinary-core-shrinkwrap.js" type="text/javascript"></script>
+<script type="text/javascript">
+    var cl = cloudinary.Cloudinary.new({cloud_name: "gerber"}); 
+    // replace 'demo' with your cloud name in the line above 
+    cl.responsive();
+</script>
