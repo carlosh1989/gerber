e07cbafb2734374d7bc101ee247864c86cddbd51
carlos silva <carloshsilva1989@gmail.com>
2018-11-23 15:06:39 -0500

    modficado y mejorado las parametros y variables

--- index.php
+++ index.php
@@ -1,25 +1,22 @@
-<?php
-$args = array(
-    'link' => '#',
-    'url_img' => 'https://res.cloudinary.com/tuconsultaenlinea/image/upload/w_auto,c_scale/v1533602773/iwevpcagpd0kuwno3xnx.jpg', 
-    'alt' => 'descripcion de imagen');
-?>
+<!-- Importacion de config.php -->
+<?php require_once('config.php') ?>
 
+<!-- Tag obligatoria para el funcionamiento de la libreria -->
+<!-- Por lo general el meta viene en ecala 1.0 pero para en este caso debe ir "device-with" -->
 <meta name="viewport" content="width=device-width">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-
-<? if ($args['link'] == '#'): ?>
-    <img data-src="<?php echo $args['url_img'] ?>" class="cld-responsive">
+<!-- Condicional para verificar si la imagen lleva una enlamce o no -->
+<? if ('#'): ?>
+    <img data-src="<?php echo $picture ?>" class="cld-responsive" alt="<?php $alt ?>">
 <? else: ?>
-    <a href="<?php $args['link'] ?>"><img data-src="<?php echo $args['url_img'] ?>" class="cld-responsive"></a>
+    <a href="<?php $link ?>"><img data-src="<?php echo $picture ?>" class="cld-responsive" alt="<?php $alt ?>"></a>
 <? endif; ?>
 
-
- 
+<!-- Paquete de CLoudinary, se debe posicionar en la carpeta raiz e instalar con el comando: "bower install cloudinary-core" -->
 <script src="bower_components/cloudinary-core/cloudinary-core-shrinkwrap.js" type="text/javascript"></script>
 <script type="text/javascript">
-    var cl = cloudinary.Cloudinary.new({cloud_name: "gerber"}); 
-    // replace 'demo' with your cloud name in the line above 
+    var cl = cloudinary.Cloudinary.new({cloud_name: "demo"}); 
+    // Reemplazar el nombre "demo" en cloud_name por el de su preferencia
     cl.responsive();
 </script>
