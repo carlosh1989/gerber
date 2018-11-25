ebcaa8fdb19ff929d0b46b72e470dcfc4e163a0a
carlos silva <carloshsilva1989@gmail.com>
2018-11-25 02:39:32 -0500

    plugi finalizado la version 1.0

--- index.php
+++ index.php
@@ -25,17 +25,21 @@
         $uri_base_account = "https://res.cloudinary.com/tuconsultaenlinea/image/upload";
         
         // Parametros obligatorios para el funcionamiento de la auto resizing de las imagenes
-        $uri_parameters_small = "f_auto,q_70,w_768";
-        $uri_parameters_medium = "f_auto,q_70,w_992";
-        $uri_parameters_large = "f_auto,q_70,w_1900";
+        $uri_parameters_extra_small = "f_auto,q_70,w_544";  #Portrait phones (<544px)
+        $uri_parameters_small       = "f_auto,q_70,w_768";  #Landscape phones (≥544px - <768px)
+        $uri_parameters_medium      = "f_auto,q_70,w_992";  #Tablets (≥768px - <992px)
+        $uri_parameters_large       = "f_auto,q_70,w_1200"; #Desktops (≥992px - <1200px)
+        $uri_parameters_extra_large = "f_auto,q_70,w_1900"; #Desktops (≥1200px)
 
         // nombre de la imagen en cloudinary 
         $uri_picture = $args['url_img'];
 
         # URL DE IMAGEN HECHA.
+        $picture_extra_small = $uri_base_account.'/'.$uri_parameters_extra_small.'/'.$uri_picture;
         $picture_small = $uri_base_account.'/'.$uri_parameters_small.'/'.$uri_picture;
         $picture_medium = $uri_base_account.'/'.$uri_parameters_medium.'/'.$uri_picture;
-        $picture_large = $uri_base_account.'/'.$uri_parameters_large.'/'.$uri_picture;  
+        $picture_large = $uri_base_account.'/'.$uri_parameters_large.'/'.$uri_picture;
+        $picture_extra_large = $uri_base_account.'/'.$uri_parameters_extra_large.'/'.$uri_picture;    
 
         #PARAMETROS DE TAG html
 
@@ -48,11 +52,11 @@
 
         if ($link == '#') {
             #$img = "<img src='".$picture."' alt='".$alt."'>";
-            $img = "<div style='width:100%'><img srcset='".$picture_small." 768w, ".$picture_medium." 992w, ".$picture_large." 1900w sizes='(max-width: 30em) 30em, 992vw src='".$picture_medium."' alt='it's responsive'/></div>";
+            $img = "<div style='width:100%'><img srcset='".$picture_extra_small." 544w, ".$picture_small." 768w, ".$picture_medium." 992w, ".$picture_large." 1200w, ".$picture_extra_large." 1900w sizes='(max-width: 30em) 30em, 992vw src='".$picture_medium."' alt='it's responsive'/></div>";
         }
         else
         {
-            $img = "<div style='width:100%'><a href='".$link."'><img srcset='".$picture_small." 768w, ".$picture_medium." 992w, ".$picture_large." 1900w sizes='(max-width: 30em) 30em, 992vw src='".$picture_medium."' alt='it's responsive'/></a></div>";
+            $img = "<div style='width:100%'><a href='".$link."'><img srcset='".$picture_extra_small." 544w, ".$picture_small." 768w, ".$picture_medium." 992w, ".$picture_large." 1200w, ".$picture_extra_large." 1900w sizes='(max-width: 30em) 30em, 992vw src='".$picture_medium."' alt='it's responsive'/></a></div>";
             #$img = "<a href='".$link."'><img data-src='".$picture."' alt='".$alt."'></a>";
         }
 
